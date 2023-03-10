402e8a9d0860b626b70fe8e5d0c9ffe14b6cfe0c
Hesenli-Hacaga <hacaga125+1@gmail.com>
2023-03-10 14:54:51 +0400

    Mobile Devices Slide to exit

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,27 @@ import { router } from './routes';
 import { store } from './store';
 
 const App = () => {
+  const containerRef = useRef(null)
+
+  function handleTouchStart(event) {
+    containerRef.current.touchStartX = event.changedTouches[0].clientX;
+  }
+
+  function handleTouchEnd(event) {
+    const touchEndX = event.changedTouches[0].clientX;
+    const touchDistance = touchEndX - containerRef.current.touchStartX;
+
+    if (touchDistance > 50) {
+      window.close()
+    }
+  }
+
   return (
     <Provider {...{ store }}>
-      <RouterProvider {...{ router }}>
-      </RouterProvider>
+      <div ref={containerRef} onTouchStart={handleTouchStart} onTouchEnd={handleTouchEnd}>
+        <RouterProvider {...{ router }}>
+        </RouterProvider>
+      </div>
     </Provider>
 
   )
